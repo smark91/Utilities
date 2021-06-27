2f4718ee5d7bb0e0c3b320c3f1c3c0b9c3467fc5
smark91 <smark1991@gmail.com>
2021-06-27 22:00:39 +0200

    remove jq dependency from create_temporary_azure_vm_nsg_rule.sh using built-in --query option

--- azure/create_temporary_azure_vm_nsg_rule.sh
+++ azure/create_temporary_azure_vm_nsg_rule.sh
@@ -6,28 +6,26 @@
 
 # Check for prerequisites
 command -v az >/dev/null 2>&1 || { echo >&2 "I require az but it's not installed.  Aborting."; exit 1; }
-command -v jq >/dev/null 2>&1 || { echo >&2 "I require jq but it's not installed.  Aborting."; exit 1; }
 command -v fzf >/dev/null 2>&1 || { echo >&2 "I require fzf but it's not installed.  Aborting."; exit 1; }
 
-
 # Interactively choose subscription, resource group and VM
-ACCOUNT=$(az account list | jq -r '.[].name' | fzf)
-RG=$(az group list --subscription "$ACCOUNT" | jq -r '.[].name' | fzf)
-VM=$(az vm list --subscription "$ACCOUNT" --resource-group "$RG" | jq -r '.[].name' | fzf)
-VM_NIC=$(az vm show --subscription "$ACCOUNT" --resource-group "$RG" --name "$VM" | jq -r '.networkProfile.networkInterfaces[0].id')
-NIC_NSG=$(az network nic show --ids "$VM_NIC" | jq -r '.networkSecurityGroup.id')
-NSG_NAME=$(az network nsg show --ids "$NIC_NSG" | jq -r '.name')
+ACCOUNT=$(az account list --query '[].name' -o tsv | fzf)
+RG=$(az group list --subscription "$ACCOUNT" --query '[].name' -o tsv | fzf)
+VM=$(az vm list --subscription "$ACCOUNT" --resource-group "$RG" --query '[].name' -o tsv | fzf)
+VM_NIC=$(az vm show --subscription "$ACCOUNT" --resource-group "$RG" --name "$VM" --query 'networkProfile.networkInterfaces[0].id' -o tsv )
+NIC_NSG=$(az network nic show --ids "$VM_NIC" --query 'networkSecurityGroup.id' -o tsv)
+NSG_NAME=$(az network nsg show --ids "$NIC_NSG" --query 'name' -o tsv)
 
 # Ask for rule values
 read -p "Rule name: " RULE_NAME
 read -p "Port ranges (e.g. 80 or 80-88 or 80,88): " PORT_RANGES
 printf  "\nCurrent rule and priority:\n"
-az network nsg rule list --subscription "$ACCOUNT" --resource-group "$RG" --nsg-name "$NSG_NAME" | jq -r '.[] | "\(.priority) \(.name)"' | sort
+az network nsg rule list --subscription "$ACCOUNT" --resource-group "$RG" --nsg-name "$NSG_NAME" --query '[].[priority, name]' -o tsv | sort -h
 read -p "Priority [100-4096]: " PRIORITY
 
 # Appling rule
 printf  "\nAppling rule..."
-az network nsg rule create --subscription "$ACCOUNT" -g "$RG" --nsg-name "$NSG_NAME" --name "$RULE_NAME" --source-address-prefixes "$(curl -s ipconfig.io)/32" --destination-port-ranges "$PORT_RANGES" --access Allow --protocol Tcp --priority "$PRIORITY" | jq -r '.provisioningState'
+az network nsg rule create --subscription "$ACCOUNT" -g "$RG" --nsg-name "$NSG_NAME" --name "$RULE_NAME" --source-address-prefixes "$(curl -s ipconfig.io)/32" --destination-port-ranges "$PORT_RANGES" --access Allow --protocol Tcp --priority "$PRIORITY" --query 'provisioningState' -o tsv
 
 # Wait for ESC input
 printf  "Press ESC key to remove the rule or CTRL-C to leave it"
